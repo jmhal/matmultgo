7a6926a0b823f23a9db415dea9d229cfcf10391e
jmhal <jmarcelo.alencar@gmail.com>
2021-02-19 09:31:52 -0300

    Added multiply code. Update language to English.

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
 # matmultgo
 
-Multiplicação de Matrizes em Go.
+Matrix Multiplication in Go.
 
-## Objetivo
+## Goal 
 
-O objetivo deste repositório é testar a organização em módulos de código em Go. Estava acostumado a usar a GOPATH, mas a metodologia foi alterada para um sistema de módulos nas últimas versões de Go. Portanto, estou criando um projeto _brinquedo_ para testara criação de um módulo.
+I created this repository to try out modules as a way of organizing Go code. I was used to use
+the GOPATH environment variable, but the methodolody in newer versions of Go has changed. Therefore, I am creating a _toy project_ to test module creation.
 
-## Referência
+## References
 
-A discussão neste [_post_](https://stackoverflow.com/questions/55442878/organize-local-code-in-packages-using-go-modules) do _StackOverflow_ motivou o meu interesse.
+The discussion in this StackOverflow's [_post_](https://stackoverflow.com/questions/55442878/organize-local-code-in-packages-using-go-modules) explains what I am testing in this repository.
--- matmultgo.go
+++ matmultgo.go
@@ -1,3 +1,56 @@
 package matmultgo
 
-var Mat int = 4
+import "time"
+
+// Creates a row major matrix
+func createMatrix(order int) []float64 {
+	var matrix []float64
+	for i := 0; i < order*order; i++ {
+		matrix = append(matrix, 1)
+	}
+	return matrix
+}
+
+// Receives two row major matrices and multiply them
+// Returns the product matrix and the elapsed time
+func matMultByInput(order int, matrix1 []float64, matrix2 []float64) ([]float64, time.Duration) {
+	matrix3 := createMatrix(order)
+
+	start := time.Now()
+	var temp float64
+	for i := 0; i < order; i++ {
+		for j := 0; j < order; j++ {
+			temp = 0
+			for k := 0; k < order; k++ {
+				temp += matrix1[i*order+k] * matrix2[k*order+j]
+			}
+			matrix3 = append(matrix3, temp)
+		}
+	}
+	elapsed := time.Since(start)
+
+	return matrix3, elapsed
+}
+
+// For bechmark purposes, multiply matrices filled with ones
+func matMultByOrder(order int) ([]float64, time.Duration) {
+	matrix1 := createMatrix(order)
+	matrix2 := createMatrix(order)
+	matrix3 := createMatrix(order)
+
+	start := time.Now()
+	var temp float64
+	for i := 0; i < order; i++ {
+		for j := 0; j < order; j++ {
+			temp = 0
+			for k := 0; k < order; k++ {
+				temp += matrix1[i*order+k] * matrix2[k*order+j]
+			}
+			matrix3 = append(matrix3, temp)
+		}
+	}
+	elapsed := time.Since(start)
+
+	return matrix3, elapsed
+}
+
